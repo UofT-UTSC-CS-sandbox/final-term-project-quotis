19e4c54c5b9c7177b1720a54ceb55b37c3a0f54c
Zuhair Khan <115852530+zuhair-mzk@users.noreply.github.com>
2024-08-03 03:44:08 -0400

    Revert "Profile address1 (#46)" (#47)
    This reverts commit 3081ecde280a8527c5bb37e0838dcdddb006993a.

--- Quotis/components/UserInfo.tsx
+++ Quotis/components/UserInfo.tsx
@@ -1,19 +1,11 @@
 import React, { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  Button,
-  StyleSheet,
-  Image,
-  Dimensions,
-} from "react-native";
+import { View, Text, Button, Image } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { RouteProp, useRoute } from "@react-navigation/native";
 import axios from "axios";
 import * as Location from "expo-location";
+import { styles } from "./UserInfoStyles";
 import { RootStackParamList } from "../../backend/src/models/types";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { FontAwesome } from "@expo/vector-icons";
 
 type UserInfoRouteProp = RouteProp<RootStackParamList, "UserInfo">;
 
@@ -68,7 +60,7 @@ const UserInfo: React.FC = () => {
         <Image
           style={styles.image}
           source={{
-            uri: photoUri || "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg",
+            uri: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Default_pfp.svg",
           }}
         />
         <Text style={styles.name}>
@@ -96,61 +88,5 @@ const UserInfo: React.FC = () => {
     </View>
   );
 };
-
-const { height, width } = Dimensions.get("window");
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    justifyContent: "flex-start",
-    alignItems: "center",
-    backgroundColor: "#f8f8f8",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  info: {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "center",
-    alignItems: "flex-start",
-    width: '45%',
-  },
-  infoTitle: {
-    fontWeight: 'light',
-  },
-  infoCont: {
-    fontWeight: 'bold',
-  },
-  banner: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-    padding: 20,
-  },
-  name: {
-    padding: 10,
-    fontWeight: "bold",
-  },
-  image: {
-    width: 100,
-    height: 100,
-    borderRadius: 50,
-    borderWidth: 1,
-    borderColor: 'black',
-  },
-  important: {
-    fontWeight: "bold",
-  },
-  line: {
-    borderBottomColor: 'black',
-    borderBottomWidth: 1,
-    height: 1,
-    marginVertical: 10,
-  },
-});
 
 export default UserInfo;
